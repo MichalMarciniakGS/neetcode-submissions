06d29dd459ca8cbcc7d4a5684aaf395b67d1f87e
MichalMarciniakGS <72413371+MichalMarciniakGS@users.noreply.github.com>
2026-05-24 14:58:31 +0200

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> maxHeap;
+        vector<int> res;
+
+        for (int i = 0; i < nums.size(); i++) {
+            maxHeap.push({nums[i], i});
+
+            while (!maxHeap.empty() && maxHeap.top().second < i - k + 1) {
+                maxHeap.pop();
+            }
+
+            if (i >= k - 1) {
+                res.push_back(maxHeap.top().first);
+            }
+        }
+
+        return res;
+    }
+};
